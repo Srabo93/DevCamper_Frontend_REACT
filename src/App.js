2490a4c511e9719b617162172a0cfc5e4cda1368
Srabo93 <A.S-dev@web.de>
2022-05-24 14:05:51 +0200

    Auth Folder created, containing needed Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import Index from "./pages/Index/Index";
 import theme from "./theme";
+import Login from "./pages/auth/Login";
 
 const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Index />
+      <Login />
     </ChakraProvider>
   );
 };
